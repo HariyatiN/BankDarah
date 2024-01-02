2cf1e1ab75ada190d36a2f13834d93039371747d
HariyatiN <hariyatiningsih124@gmail.com>
2024-01-02 22:18:20 +0700

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,7 @@
                 </div>
             </div>
         </div>
+        
         <div class="col-sm-6 col-xl-3">
             <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
                 <i class="fa fa-users fa-3x text-primary"></i>
